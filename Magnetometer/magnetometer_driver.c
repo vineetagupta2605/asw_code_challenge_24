a73bcbe3f882b2b4f2c3b24660b01cbbabf731a9
Vineeta2605 <vineetagupta1926@gmail.com>
2024-04-09 20:35:45 +0200

    Magnetometer driver for coding challenge

---
+++ Magnetometer/magnetometer_driver.c
@@ -0,0 +1,134 @@
+/*
+ * lis3mdl.c
+ *
+ * This file contains the implementation of functions for interacting with
+ * the LIS3MDLTR 3-Axis Magnetometer sensor.
+ *
+ * Created on: April 9, 2024
+ * Author: Vineeta Gupta
+ */
+
+/******************************************************************************
+ * #1 Include Header Files
+ ******************************************************************************/
+#include "../Magnetometer/magnetometer_driver.h"
+
+/******************************************************************************
+ * #8 Extern Function Definitions
+ ******************************************************************************/
+extern status_t mmt_drv_getFullScaleConfig(mmt_drv_configEn * fullScaleConfigValue_pen)
+{
+	uint8_t readBuffer_u8;
+
+	status_t status = i2c_read(MMT_DRV_I2C_BUS_ADDRESS, MMT_DRV_CTRL_REG2, 1u, &readBuffer_u8);
+	if (status == STATUS_OK)
+	{
+		uint8_t FullScaleConfigValue = (readBuffer_u8 >> 6) & 0x03;
+		switch(FullScaleConfigValue)
+		{
+		case 0x00:
+			*fullScaleConfigValue_pen = MMT_DRV_CONFIG_4G;
+			break;
+		case 0x01:
+			*fullScaleConfigValue_pen = MMT_DRV_CONFIG_8G;
+			break;
+		case 0x10:
+			*fullScaleConfigValue_pen = MMT_DRV_CONFIG_12G;
+			break;
+		case 0x11:
+			*fullScaleConfigValue_pen = MMT_DRV_CONFIG_16G;
+			break;
+		default:
+			*fullScaleConfigValue_pen = MMT_DRV_CONFIG_ERROR;
+			break;
+	}
+
+	}
+	return status;
+}
+
+
+extern status_t mmt_drv_getDataRate(mmt_drv_SpeedConfigurationSt * dataRate_pst)
+{
+	uint8_t registerBuffer_u8;
+
+	status_t status = i2c_read(MMT_DRV_I2C_BUS_ADDRESS, MMT_DRV_CTRL_REG1, 1u, &registerBuffer_u8);
+	if (status == STATUS_OK)
+	{
+		dataRate_pst->dataRate = (registerBuffer_u8 & MMT_DRV_SPEED_MASK) >> 2;
+		dataRate_pst->mode = (registerBuffer_u8 & 0x03);
+		dataRate_pst->fastOdr = (registerBuffer_u8 & MMT_DRV_FAST_ODR_MASK);
+	}
+
+	return status;
+}
+
+
+extern status_t mmt_drv_setDataRate(mmt_drv_SpeedConfigurationSt dataRate_pst)
+{
+	uint8_t registerBuffer_u8 = ((dataRate_pst.dataRate << 2) & MMT_DRV_SPEED_MASK) | (dataRate_pst.mode & 0x03) | (dataRate_pst.fastOdr & MMT_DRV_FAST_ODR_MASK);
+	status_t status = i2c_write(MMT_DRV_I2C_BUS_ADDRESS, MMT_DRV_CTRL_REG1, 1u, &registerBuffer_u8);
+
+	return status;
+}
+
+
+extern status_t mmt_drv_setInterrupt(bool userInterruptState_b)
+{
+	uint8_t readBuffer_u8;
+	status_t status = i2c_read(MMT_DRV_I2C_BUS_ADDRESS, MMT_DRV_INT_CFG, 1u, &readBuffer_u8);
+
+	if (status == STATUS_OK)
+	{
+		bool interruptStatus_u8 = (readBuffer_u8 & 0x01) != 0;
+
+		if (userInterruptState_b == true && interruptStatus_u8 == false)
+		{
+			readBuffer_u8 |= 0x01;
+			status = i2c_write(MMT_DRV_I2C_BUS_ADDRESS, MMT_DRV_INT_CFG, 1u, &readBuffer_u8);
+		}
+
+		else if (userInterruptState_b == false && interruptStatus_u8 == true)
+		{
+			readBuffer_u8 &= ~0x01;
+			status = i2c_write(MMT_DRV_I2C_BUS_ADDRESS, MMT_DRV_INT_CFG, 1u, &readBuffer_u8);
+		}
+		else
+		{
+			/* Do nothing because it is already set as requested by the user */
+		}
+	}
+
+	return status;
+}
+
+extern status_t mmt_drv_readOutputData(mmt_drv_AxisEn axis_en, int16_t *outputData)
+{
+    uint8_t lowReg_u8, highReg_u8;
+    status_t status = STATUS_OK;
+    uint8_t regAddress_u8;
+
+    switch (axis_en) {
+        case MMT_DRV_X_AXIS:
+        	regAddress_u8 = MMT_DRV_X_L;
+            break;
+        case MMT_DRV_Y_AXIS:
+        	regAddress_u8 = MMT_DRV_Y_L;
+            break;
+        case MMT_DRV_Z_AXIS:
+        	regAddress_u8 = MMT_DRV_Z_L;
+            break;
+        default:
+            // Return error if axis is invalid
+    }
+
+    status |= i2c_read(MMT_DRV_I2C_BUS_ADDRESS, regAddress_u8, 1u, &lowReg_u8);
+    status |= i2c_read(MMT_DRV_I2C_BUS_ADDRESS, regAddress_u8 + 1, 1u, &highReg_u8);
+
+    if (status == STATUS_OK)
+    {
+    	*outputData = (int16_t)((highReg_u8 << 8) | lowReg_u8);
+    }
+
+    return status;
+}
---
+++ Magnetometer/magnetometer_driver.h
@@ -0,0 +1,141 @@
+/*
+ * magnetometer_driver.h
+ *
+ * This file contains the definitions for interacting with
+ * the LIS3MDLTR 3-Axis Magnetometer sensor.
+ *
+ * Created on: April 9, 2024
+ * Author: Vineeta Gupta
+ */
+
+
+#ifndef MAGNETOMETER_DRIVER_H_
+#define MAGNETOMETER_DRIVER_H_
+
+/******************************************************************************
+ * #1 Include Header Files
+ ******************************************************************************/
+#include <stdlib.h>
+#include <stdbool.h>
+#include <main.h>
+#include <i2c.h>
+
+/* Dummy include for compilation at personal system */
+#include <comm_i2c.h>
+
+/******************************************************************************
+ * #2 Macro Declarations
+ ******************************************************************************/
+/* Dummy bus address */
+#define MMT_DRV_I2C_BUS_ADDRESS		0x50
+
+/* Necessary bit masks */
+#define MMT_DRV_SPEED_MASK			0x1C
+#define MMT_DRV_FAST_ODR_MASK		0x01
+
+/* Register mapping of required registers */
+#define MMT_DRV_CTRL_REG1	0x20
+#define MMT_DRV_CTRL_REG2	0x21
+#define MMT_DRV_X_L			0x28
+#define MMT_DRV_X_H			0x29
+#define MMT_DRV_Y_L			0x2A
+#define MMT_DRV_Y_H			0x2B
+#define MMT_DRV_Z_L			0x2C
+#define MMT_DRV_Z_H			0x2D
+#define MMT_DRV_INT_CFG		0x30
+
+/******************************************************************************
+ * #3 Type Declarations
+ ******************************************************************************/
+typedef enum {
+	MMT_DRV_CONFIG_4G,
+	MMT_DRV_CONFIG_8G,
+	MMT_DRV_CONFIG_12G,
+	MMT_DRV_CONFIG_16G,
+	MMT_DRV_CONFIG_ERROR,
+} mmt_drv_configEn;
+
+typedef enum {
+	MMT_DRV_RATE_0_625_Hz,
+	MMT_DRV_RATE_1_25_Hz,
+	MMT_DRV_RATE_2_25_Hz,
+	MMT_DRV_RATE_5_Hz,
+	MMT_DRV_RATE_10_Hz,
+	MMT_DRV_RATE_20_Hz,
+	MMT_DRV_RATE_40_Hz,
+	MMT_DRV_RATE_80_Hz
+} mmt_drv_OutputDataRateEn;
+
+typedef enum {
+	MMT_DRV_LP_MODE,
+	MMT_DRV_MP_MODE,
+	MMT_DRV_HP_MODE,
+	MMT_DRV_UHP_MODE
+} mmt_drv_OperatingModeEn;
+
+typedef enum {
+	MMT_DRV_X_AXIS,
+	MMT_DRV_Y_AXIS,
+	MMT_DRV_Z_AXIS,
+} mmt_drv_AxisEn;
+
+typedef struct {
+	mmt_drv_OutputDataRateEn dataRate;
+	mmt_drv_OperatingModeEn mode;
+    uint8_t fastOdr;
+} mmt_drv_SpeedConfigurationSt;
+
+/******************************************************************************
+ * #8 Extern Function Declaration
+ ******************************************************************************/
+/**
+ * @brief Get the full-scale configuration of the magnetometer.
+ *
+ * @param[out] fullScaleConfigValue_pen Pointer to a variable to store the full-scale configuration value.
+ *
+ * @return STATUS_OK if successful.
+ */
+extern status_t mmt_drv_getFullScaleConfig(mmt_drv_configEn * fullScaleConfigValue_pen);
+
+
+/**
+ * @brief Get the data rate configuration of the magnetometer.
+ *
+ * @param[out] dataRate_pst Pointer to a SpeedConfiguration struct to store the data rate configuration.
+ *
+ * @return STATUS_OK if successful.
+ */
+extern status_t mmt_drv_getDataRate(mmt_drv_SpeedConfigurationSt * dataRate_pst);
+
+
+/**
+ * @brief Set the data rate configuration of the magnetometer.
+ *
+ * @param[in] dataRate_pst SpeedConfiguration struct containing the desired data rate configuration.
+ *
+ * @return STATUS_OK if successful
+ */
+extern status_t mmt_drv_setDataRate(mmt_drv_SpeedConfigurationSt dataRate_pst);
+
+
+/**
+ * @brief Enable or disable the interrupt pin of the magnetometer.
+ *
+ * @param[in] userInterruptState_b Boolean value indicating whether to enable (true) or disable (false) the interrupt pin.
+ *
+ * @return STATUS_OK if successful.
+ */
+extern status_t mmt_drv_setInterrupt(bool userInterruptState_b);
+
+
+/**
+ * @brief Read the output data of a specified axis from the magnetometer.
+ *
+ * @param[in] axis_en 			Axis for which to read the output data (X, Y, or Z).
+ * @param[out] outputData_p16 	Pointer to a variable to store the output data.
+ *
+ * @return STATUS_OK if successful.
+ */
+extern status_t mmt_drv_readOutputData(mmt_drv_AxisEn axis_en, int16_t * outputData_p16);
+
+#endif /* MAGNETOMETER_DRIVER_H_ */
